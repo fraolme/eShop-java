36571a35205259c59baac95e29f35b9c781720f5
Fraol <88104941+fraolme@users.noreply.github.com>
2024-08-07 20:37:56 +0300

    basket service: missed changed

--- services/basket/src/main/java/io/github/fraolme/services/basket/config/auth/SecurityConfig.java
+++ services/basket/src/main/java/io/github/fraolme/services/basket/config/auth/SecurityConfig.java
@@ -1,2 +1,36 @@
-package io.github.fraolme.services.basket.config.auth;public class SecurityConfig {
+package io.github.fraolme.services.basket.config.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+    };
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.oauth2ResourceServer((oauth2) -> oauth2
+                .jwt(Customizer.withDefaults())
+        );
+
+        return http.build();
+    }
 }
+
+
