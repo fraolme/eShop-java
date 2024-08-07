3d9a1280662729462df60277e1d232809b2a2345
Fraol <88104941+fraolme@users.noreply.github.com>
2024-08-07 20:49:09 +0300

    basket servie: retrive user id and usename from jwt claims

--- services/basket/src/main/java/io/github/fraolme/services/basket/controllers/BasketController.java
+++ services/basket/src/main/java/io/github/fraolme/services/basket/controllers/BasketController.java
@@ -4,9 +4,10 @@ import io.github.fraolme.services.basket.models.BasketCheckout;
 import io.github.fraolme.services.basket.models.CustomerBasket;
 import io.github.fraolme.services.basket.repositories.RedisBasketRepository;
 import io.github.fraolme.services.basket.utils.UuidUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,15 +41,16 @@ public class BasketController {
     }
 
     @PostMapping("checkout")
-    public void checkout(@RequestBody BasketCheckout basketCheckout, @RequestHeader("x-requestid") String requestIdFromHeader) {
-        // TODO: get customer id from bearer token
-        String customerId = "1";
+    public void checkout(@RequestBody BasketCheckout basketCheckout, @RequestHeader("x-requestid") String requestIdFromHeader,
+                         @AuthenticationPrincipal Jwt jwt) {
+
+        String customerId = jwt.getClaim("sub");
         UUID requestId = UuidUtils.tryParseUUID(requestIdFromHeader).orElse(basketCheckout.RequestId());
 
         var basket = this.redisBasketRepository.findById(customerId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
 
-        // TODO: get username from bearer token
+        var username = jwt.getClaim("preferred_username");
 
         // TODO: send UserCheckoutAcceptedIntegrationEvent
     }
